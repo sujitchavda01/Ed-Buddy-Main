04d1a84cbd73382f05a5e1f5b4ed82c6906eb010
sujitchavda01 <sujitchavda01@gmail.com>
2025-06-01 20:10:58 +0530

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { SpeedInsights } from "@vercel/speed-insights/vue"
 
 const queryClient = new QueryClient();
 
@@ -13,7 +12,6 @@ const App = () => (
   
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <SpeedInsights/>
       <Toaster />
       <Sonner/>
       <BrowserRouter>
